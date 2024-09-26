f62fd9152f4e076caea5ac087a4ce0f6268fb1f7
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-26 14:03:22 +0300

    removed unused imports

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Accordion = ({children, className}) => {
     return (
         <ul className={className}>
--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const AccordionItem = ({className, title, children}) => {
     return (
         <li className={className}>
